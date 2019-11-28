668e535d663e8b56626512199e93b054c25a4743
Łukasz <Lukasz19281@gmail.com>
2019-11-28 15:04:01 +0100

    Basic logic implemented.

--- src/main/java/app/MainApp.java
+++ src/main/java/app/MainApp.java
@@ -7,11 +7,71 @@ import org.javatuples.Pair;
 import processor.MapProcessingUtil;
 import transformation.Transformation;
 import transformation.TransformationP1;
+import transformation.TransformationP2;
 import transformation.TransformationP7;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainApp {
 
     private static Logger log = Logger.getLogger(MainApp.class.getName());
+
+    private static void testTransformationP2() {
+        ModelGraph graph = new ModelGraph("testGraph");
+        Vertex v1 = graph.insertVertex("v1", VertexType.SIMPLE_NODE, new Point3d(0.0, 0.0, 0.0));
+        Vertex v2 = graph.insertVertex("v2", VertexType.SIMPLE_NODE, new Point3d(100.0, 0.0, 0.0));
+        Vertex v3 = graph.insertVertex("v3", VertexType.SIMPLE_NODE, new Point3d(200.0, 0.0, 0.0));
+        Vertex v4 = graph.insertVertex("v4", VertexType.HANGING_NODE, new Point3d(50.0, 50.0, 0.0));
+        Vertex v5 = graph.insertVertex("v5", VertexType.HANGING_NODE, new Point3d(150.0, 50.0, 0.0));
+        Vertex v6 = graph.insertVertex("v6", VertexType.SIMPLE_NODE, new Point3d(0.0, 100.0, 0.0));
+        Vertex v7 = graph.insertVertex("v7", VertexType.SIMPLE_NODE, new Point3d(100.0, 100.0, 0.0));
+        Vertex v8 = graph.insertVertex("v8", VertexType.SIMPLE_NODE, new Point3d(200.0, 100.0, 0.0));
+
+        graph.insertEdge("e1", v1, v2, true);
+        graph.insertEdge("e2", v2, v3, true);
+        graph.insertEdge("e3", v1, v4, false);
+        graph.insertEdge("e4", v2, v4, false);
+        graph.insertEdge("e5", v2, v7, false);
+        graph.insertEdge("e6", v2, v5, false);
+        graph.insertEdge("e7", v3, v8, true);
+        graph.insertEdge("e8", v1, v6, true);
+        graph.insertEdge("e9", v4, v6, false);
+        graph.insertEdge("e10", v5, v7, false);
+        graph.insertEdge("e11", v5, v8, false);
+        graph.insertEdge("e12", v6, v7, true);
+        graph.insertEdge("e13", v7, v8, true);
+
+        List<InteriorNode> ints = new ArrayList<>();
+        ints.add(graph.insertInterior("i1", v1, v2, v4));
+        ints.add(graph.insertInterior("i2", v1, v4, v6));
+        ints.add(graph.insertInterior("i3", v2, v6, v7));
+        ints.get(2).setPartitionRequired(true);
+        ints.add(graph.insertInterior("i4", v2, v5, v7));
+        ints.add(graph.insertInterior("i5", v5, v7, v8));
+        ints.add(graph.insertInterior("i6", v2, v3, v8));
+        ints.get(5).setPartitionRequired(true);
+
+        graph.display();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        int i = 0;
+        for(InteriorNode _i : ints) {
+            Transformation t2 = new TransformationP2();
+            if (t2.isConditionCompleted(graph, _i)) {
+                System.out.println("true ".concat(Integer.toString(i)));
+                t2.transformGraph(graph, _i);
+            }
+            else {
+                System.out.println("false ".concat(Integer.toString(i)));
+            }
+            i++;
+        }
+    }
 
     private static Pair<ModelGraph, InteriorNode> task1() {
         ModelGraph graph = new ModelGraph("testGraph");
@@ -120,21 +180,23 @@ public class MainApp {
     public static void main(String[] args) {
         BasicConfigurator.configure();
 
-        Pair<ModelGraph, InteriorNode> task = task1();
-        ModelGraph graph = task.getValue0();
-        InteriorNode interiorNode = task.getValue1();
+        testTransformationP2();
 
-        Transformation t1 = new TransformationP1();
-        log.info(String.format("Condition state for transformation P1: %b", t1.isConditionCompleted(graph, interiorNode)));
-
-        graph.display();
-
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        t1.transformGraph(graph, interiorNode);
+//        Pair<ModelGraph, InteriorNode> task = task1();
+//        ModelGraph graph = task.getValue0();
+//        InteriorNode interiorNode = task.getValue1();
+//
+//        Transformation t1 = new TransformationP1();
+//        log.info(String.format("Condition state for transformation P1: %b", t1.isConditionCompleted(graph, interiorNode)));
+//
+//        graph.display();
+//
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        t1.transformGraph(graph, interiorNode);
 
 //        TerrainMap map = new TerrainMap();
 //        map.fillMapWithExampleData();
